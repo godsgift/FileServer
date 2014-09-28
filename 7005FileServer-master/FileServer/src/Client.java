2f0c27cd6f5dee8078fff9bca35a06975f9f39fc
godsgift <eltonsia@yahoo.com>
2014-09-27 20:28:16 -0700

    fileserver
    test

---
+++ 7005FileServer-master/FileServer/src/Client.java
@@ -0,0 +1,194 @@
+
+import java.net.Socket;
+import java.io.*;
+import static java.time.Clock.system;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+ 
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Thilina Ratnayake (A00802338) & Elton Sia 
+ */
+public class Client {
+    static String send = "SEND";
+    static String get = "GET";
+    
+    public static void main(String[] args) throws Exception{
+        
+        //Elton: Handle User Input here so they can either so SEND (FILENAME) or GET(FILENAME). Probably have to use a switch.
+        System.out.println("Note: it has to be all capital letters, and server must be restarted after a command has been sent");
+        System.out.println("Type SEND to send a file otherwise type GET to receive a file");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        if (input.equals(send))
+        {
+        
+        //Send the file, three args are: file name, server address and port,
+        Client.SEND("send.txt", "127.0.0.1", 7005);
+        
+        }
+        else if(input.equals(get))
+        {
+            Client.GET("get.txt", "127.0.0.1", 7005);
+        }
+           
+                     
+        
+    }
+    
+    public static void SEND(String sendFileName, String serverAddress,int ServerPort)
+    {
+            
+        try {
+            //Find the file you want to send
+            File file = new File(sendFileName);
+            //Create the socket that will deal with the connection
+            Socket ServerSock = new Socket(serverAddress, 7005);
+                //Debug
+                //System.out.println("Local port is:");
+                //System.out.println(ServerSock.getLocalPort());
+                   
+            String COMMAND = "SEND,"+file.getName();
+            
+            System.out.println(COMMAND);
+           
+            //Send the FileTransfer command to the server
+            try {
+                ServerSock.getOutputStream().write(COMMAND.getBytes());
+                ServerSock.getOutputStream().flush();
+            }
+            
+            catch (Exception e){
+                
+            }
+            
+            //Get the response from the server
+            byte[] Container = new byte[80];
+            ServerSock.getInputStream().read(Container);
+            //Encode bits with UTF so we can read it as Humans
+            String Response = new String(Container, "UTF-8");
+            //Remove whitespace (because we don't use all of the 80 bits)
+            Response = Response.trim();
+                    //DEBUG Print out the contents of the command
+                    //System.out.println("Response from server is "+ Response);
+            
+            //Server sends back MESSAGE,PORT to initiate connection with.
+            String[] responseMessageElements = Response.split(",");
+            
+                    //DEBUG: Check slot 1 which SHOULD contain the port.
+            System.out.println("Reponse Message Elements "+ responseMessageElements[1]);
+            
+            //The port used for file transfer operations as sent from Server
+            int ftport = Integer.parseInt(responseMessageElements[1]);
+            
+            //Create the new Socket for File Transfer
+            Socket fileTransferSock = new Socket(serverAddress, ftport);
+            
+            //Create a new buffer array to hold the file
+            byte[] bufferByteArray = new byte[(int) file.length()];
+            
+            try {
+                //Create a file input stream to take in the file
+                FileInputStream fInput = new FileInputStream(file);
+                
+                //Create a buffered input stream to read that file input stream
+                BufferedInputStream bInput = new BufferedInputStream(fInput);
+                
+                //Read the file through the buffered input stream until the length of the file
+                bInput.read(bufferByteArray, 0,bufferByteArray.length);
+                
+                //Make an output stream to write the file to the socket
+                OutputStream os = fileTransferSock.getOutputStream();
+                
+                //Write everything in buffer to outputstream, until the size of the outputstream
+                os.write(bufferByteArray, 0, bufferByteArray.length);
+
+                //flush the output stream
+                os.flush();
+                
+                //Close the socket
+                fileTransferSock.close();
+                
+                //DEBUG
+                System.out.println("File has been written to the output stream and the socket is closed.");
+
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+            //Create a new buffer array to hold the file
+            
+    }
+    
+    public static void GET(String getFileName, String address, int port){
+        int newPort;
+        try{
+            
+        Socket socket = new Socket(address, port);
+        
+        File file = new File(getFileName);
+        String command ="GET," + file.getName();
+        
+        System.out.println(command);
+ 
+        socket.getOutputStream().write(command.getBytes());
+        socket.getOutputStream().flush();
+        byte[] bit = new byte[80];
+        socket.getInputStream().read(bit);
+            
+        String change = new String(bit, "UTF-8");
+        
+        //replace white spaces
+        change = change.trim();
+        
+        System.out.println("change " + change);
+        
+        String[] Elements = change.split(",");          
+            
+        newPort = Integer.parseInt(Elements[1]);
+
+        Socket newSock = new Socket(address, newPort);
+        
+        byte[] container = new byte[1048];
+            try{
+        InputStream is = newSock.getInputStream();
+   
+        int fileSize = is.read(container,0,container.length);
+         
+        FileOutputStream fout = new FileOutputStream("./clientsavedir/getfile.txt");
+
+        BufferedOutputStream boutput = new BufferedOutputStream(fout);
+
+        boutput.write(container,0,fileSize);
+        
+        boutput.close();
+        
+        System.out.println("File Saved to clientsavedir directory"); 
+            }
+            catch(Exception e) {
+                
+            }
+            
+        }
+        catch (IOException | NumberFormatException e){
+            
+        }
+
+        
+    }
+}
---
+++ 7005FileServer-master/FileServer/src/Server.java
@@ -0,0 +1,244 @@
+import java.io.*;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author thilinaratnayake
+ */
+public class Server {
+    
+    private static int port= 7005;
+    private static boolean started = true;
+    File fileToSend = new File("alice.txt");
+    private static String FileCommand = null;
+    
+    public Server (int port){
+        this.port = port;
+    }
+    
+    public static void main(String[] args) {
+        //Create the new server on port 7005
+        System.out.println("Start @ Main");
+        Server server1 = new Server(7005);  
+        //Start the server
+        System.out.println("serverStart");
+        server1.serverStart();
+        
+    }
+    
+    public static void serverStart(){
+                    
+                   
+                    
+        try {
+            
+            
+            ServerSocket servsocket = new ServerSocket(port);
+            System.out.println("Server started on port "+port);
+            
+            while(started){
+                Socket socket = servsocket.accept();
+                
+                try{
+                    System.out.println("Connection Successful!");
+                    
+                    //Choose what Client wants server to do, Either go into GET or SEND
+                    Server.ReadCommand(socket);	
+                }    
+                catch (Exception e){
+                    System.out.println("Exception is " +e);
+                }
+            }
+        }
+        
+        catch(IOException e){
+            String msg = " Exception on new ServerSocket: " + e + "\n";
+			         System.out.println(msg);
+        }
+    }
+    
+    
+    public static void ReadCommand(Socket socket){
+        try {
+                //Make a container (byte array) to hold incoming bits of the COMMAND
+                //Size 80 is arbitrary
+                byte[] Container = new byte[80];
+                //Read the inputstream into the Container
+                socket.getInputStream().read(Container);
+                        //DEBUG: See raw data thats been captured
+                        //System.out.println(Container.toString());
+               
+                //Encode bits with UTF so we can read it as Humans
+                String Command = new String(Container, "UTF-8");
+                //Remove whitespace (because we don't use all of the 80 bits)
+                Command = Command.replaceAll("//s+","");
+                    //DEBUG Print out the contents of the command
+                System.out.println("Command received is "+Command);
+                String[] CommandElements = Command.split(",");
+                //Debug:System.out.println(CommandElements[0]);
+                FileCommand = CommandElements[0];
+                String FileName = CommandElements[1];
+                
+                //Debug System.out.println(CommandElements[2]);
+                
+                switch(FileCommand){
+                    //If CLIENT is trying to SEND, then get the address and invoke Servers GET
+                    case "SEND": {
+                        InetAddress addr = socket.getInetAddress();
+                        System.out.println("SEND has been invoked from Address "+ addr+socket.getPort());
+                        
+                        try {
+                            //Create new Socket to wait for File Transfers
+                            ServerSocket FileSocket = new ServerSocket(8006);
+                            System.out.println("Socket 8006 has been created for a file transfer");
+                            //Send RESPONSE message
+                            socket.getOutputStream().write(("SENDFILE,"+FileSocket.getLocalPort()).getBytes());
+                            System.out.println("Response message sent to the client");
+                            //When Client reconnects to send file
+                            boolean receiveMode = true;
+                            while(receiveMode){
+                                System.out.println("Awaiting connections");
+                            Socket incomingSocket = FileSocket.accept();
+                                System.out.println("File Transfer Connection established");
+                            Server.GET(incomingSocket,FileName);
+                            
+                            }
+                            
+                            
+                        } catch (IOException ex) {
+                            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    case "GET": {
+                        System.out.println("Get is now invoked");
+                        try{
+                             ServerSocket servSock = new ServerSocket(5005);
+                             System.out.println("Socket 5005 has been created for a file transfer");
+                             socket.getOutputStream().write(("GETFILE," + servSock.getLocalPort()).getBytes());
+                             System.out.println("Response message sent to the client");
+                        
+                              boolean sendMode = true;
+                              
+                              while(sendMode){
+                                 System.out.println("Awaiting connections");
+                                 Socket outgoingSocket = servSock.accept();
+                                 System.out.println("File transfer connection established");
+                                 Server.SEND(outgoingSocket, FileName);
+                                }
+                              
+                        }
+                        catch(IOException ex)
+                        {
+                            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }
+         
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    
+    public static void GET(Socket incomingSocket, String FileName){
+       
+        
+        System.out.println("Starting GET");
+        //Debug
+         //System.out.println("Start writing file");
+        //Choose your incoming socket
+       //Socket socket = incomingSocket;
+        //Define buffer size
+       byte[] bufferByteArray = new byte[1048];
+        
+        try {
+            // Get the inputstream from socket
+            InputStream is = incomingSocket.getInputStream();
+            // Read the number of bytes that need to be read.
+            int fileSizeInBits = is.read(bufferByteArray, 0, bufferByteArray.length);
+                
+                //Debug: Print file size
+                //System.out.println(fileSizeInBits);
+            
+        
+            // Create a new file output stream to handle writing the file onto the server.
+            FileOutputStream foutput = new FileOutputStream("./savedir/NewFile.txt");
+            // Use a buffered output stream that writes to the file output stream
+            BufferedOutputStream boutput = new BufferedOutputStream(foutput);
+            
+            //Write the file
+            boutput.write(bufferByteArray, 0, fileSizeInBits);
+            
+            
+           //CLose the buffer
+            boutput.close();
+            System.out.println("File saved to /savedir");
+           //Close the socket
+            //System.out.println("Before close");
+            incomingSocket.close();
+  
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+    }
+    
+    public static void SEND(Socket outgoingSocket, String FileName)
+    {
+        
+        try{
+            System.out.println("Starting Send");
+            //finds out which file tosend
+            File file = new File(FileName);
+            //where the file path is
+            String abpath = file.getAbsolutePath();
+            String fpath = abpath.substring(0,abpath.lastIndexOf(File.separator));
+            System.out.println("filepath is " +fpath);
+            //creates buffer array to hold file
+            byte[] bit = new byte[(int) file.length()];
+            System.out.println("File is " + file);
+            System.out.println("File name is " + FileName);
+            //create a file input stream to take in the file
+            FileInputStream  fis = new FileInputStream(file); 
+                System.out.println(fis);
+            //create buffered input stream to read that file input stream
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            
+            //read the file through the buffered input stream until the length of the file
+            bis.read(bit, 0, bit.length);
+            
+            //make an output stream to write the file  to the socket
+            OutputStream os = outgoingSocket.getOutputStream();
+               
+            //write everything in buffer to outputstream, until the size of the outputstream
+            os.write(bit, 0, bit.length);
+                System.out.println("file has been written");
+                            
+                //flush the output stream
+            os.flush();
+            
+            
+            //close the socket
+            outgoingSocket.close();
+            
+            //test
+            System.out.println("File has been written to output stream and socket is closed");
+            }
+            
+            catch (FileNotFoundException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+    
+}
